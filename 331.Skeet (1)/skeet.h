6ef94ab347959123c2f2f8ecef8b81906f26b9dc
tre23014 <tre23014@byui.edu>
2026-04-02 22:21:14 -0600

    Add ogstream to Skeet class
    Added ogstream for output handling.

--- 331.Skeet (1)/skeet.h
+++ 331.Skeet (1)/skeet.h
@@ -18,6 +18,7 @@
 #include "time.h"
 #include "score.h"
 #include "points.h"
+#include "ogstream.h"
 
 #include <list>
 
@@ -62,4 +63,5 @@ private:
     HitRatio hitRatio;             // the hit ratio for the birds
     Position dimensions;           // size of the screen
     bool bullseye;
+    ogstream gout;
 };
