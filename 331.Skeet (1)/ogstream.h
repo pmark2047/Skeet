14e79e9b8b19bc4849de2340fa0f9bf999dfc8d5
tre23014 <tre23014@byui.edu>
2026-04-02 22:23:47 -0600

    Add ogstream class for graphical output

---
+++ 331.Skeet (1)/ogstream.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "position.h"
+#include <string>
+
+class ogstream
+{
+public:
+   ogstream() {}
+
+   // set position 
+   void setPosition(const Position & pt);
+
+   // drawing methods (Facade interface)
+   void drawText(const Position & topLeft, const std::string & text) const;
+   void drawLine(const Position & begin, const Position & end) const;
+   void drawDot(const Position & pt) const;
+
+   // flush 
+   void flush() const;
+
+private:
+   Position pt;
+};
