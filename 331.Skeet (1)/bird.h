3d58506758cb2621ae954e7ec6ec4dd4eb7b4688
tre23014 <tre23014@byui.edu>
2026-04-02 22:47:30 -0600

    Refactor Bird class to include ogstream in draw method

--- 331.Skeet (1)/bird.h
+++ 331.Skeet (1)/bird.h
@@ -9,6 +9,7 @@
 
 #pragma once
 #include "position.h"
+#include "ogstream.h"  
 
 /**********************
  * BIRD
@@ -18,27 +19,27 @@ class Bird
 {
 protected:
    static Position dimensions; // size of the screen
-   Position pt;                  // position of the flyer
-   Velocity v;                // velocity of the flyer
-   double radius;             // the size (radius) of the flyer
-   bool dead;                 // is this flyer dead?
-   int points;                // how many points is this worth?
+   Position pt;                // position of the flyer
+   Velocity v;                 // velocity of the flyer
+   double radius;              // the size (radius) of the flyer
+   bool dead;                  // is this flyer dead?
+   int points;                 // how many points is this worth?
    
 public:
    Bird() : dead(false), points(0), radius(1.0) { }
    
    // setters
-   void operator=(const Position    & rhs) { pt = rhs;    }
-   void operator=(const Velocity & rhs) { v = rhs;     }
+   void operator=(const Position & rhs) { pt = rhs; }
+   void operator=(const Velocity & rhs) { v = rhs; }
    void kill()                          { dead = true; }
-   void setPoints(int pts)              { points = pts;}
+   void setPoints(int pts)              { points = pts; }
 
    // getters
-   bool isDead()           const { return dead;   }
-   Position getPosition()     const { return pt;     }
-   Velocity getVelocity()  const { return v;      }
-   double getRadius()      const { return radius; }
-   int getPoints() const { return points; }
+   bool isDead() const           { return dead; }
+   Position getPosition() const  { return pt; }
+   Velocity getVelocity() const  { return v; }
+   double getRadius() const      { return radius; }
+   int getPoints() const         { return points; }
    bool isOutOfBounds() const
    {
       return (pt.getX() < -radius || pt.getX() >= dimensions.getX() + radius ||
@@ -46,54 +47,50 @@ public:
    }
 
    // special functions
-   virtual void draw() = 0;
+   virtual void draw(ogstream & gout) = 0;  
    virtual void advance() = 0;
 };
 
 /*********************************************
  * STANDARD
- * A standard bird: slows down, flies in a straight line
  *********************************************/
 class Standard : public Bird
 {
 public:
     Standard(double radius = 25.0, double speed = 5.0, int points = 10);
-    void draw();
+    void draw(ogstream & gout);
     void advance();
 };
 
 /*********************************************
  * FLOATER
- * A bird that floats like a balloon: flies up and really slows down
  *********************************************/
 class Floater : public Bird
 {
 public:
     Floater(double radius = 30.0, double speed = 5.0, int points = 15);
-    void draw();
+    void draw(ogstream & gout);
     void advance();
 };
 
 /*********************************************
  * CRAZY
- * A crazy flying object: randomly changes direction
  *********************************************/
 class Crazy : public Bird
 {
 public:
     Crazy(double radius = 30.0, double speed = 4.5, int points = 30);
-    void draw();
+    void draw(ogstream & gout);
     void advance();
 };
 
 /*********************************************
  * SINKER
- * A sinker bird: honors gravity
  *********************************************/
 class Sinker : public Bird
 {
 public:
     Sinker(double radius = 30.0, double speed = 4.5, int points = 20);
-    void draw();
+    void draw(ogstream & gout);
     void advance();
 };
